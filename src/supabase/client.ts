29f2427f113ae7945c0742057e7534342be6c712
salil koirala <salil@contactspace.com>
2025-02-10 18:34:19 +1100

    remove files

--- src/supabase/client.ts
+++
@@ -1,6 +0,0 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
-
-const supabaseUrl: string = 'https://hhhrpbqjtotswvmtcbyz.supabase.co';  // Replace with your Supabase URL
-const supabaseAnonKey: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoaHJwYnFqdG90c3d2bXRjYnl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxNTI5MDYsImV4cCI6MjA1NDcyODkwNn0.rQ6O3b6g5dXJJskJXH8gtfJHXFpRKSuHLqJST5mqFX8';  // Replace with your Supabase anon key
-
-export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
